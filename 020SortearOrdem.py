e6ea3e0ad1510715823af0a056b559e69da85405
RicardoMagalhaesRFinkelstein <ricardomrfin@gmail.com>
2022-07-29 13:17:46 -0300

    Exercício de sorteio grupo resolvido

--- 020SortearOrdem.py
+++ 020SortearOrdem.py
@@ -1,1 +1,13 @@
 #Num universo de 4 alunos, sortear a ordem de apresentação deles
+
+import random
+
+aluno1 = input('Digite o nome do primeiro aluno: ')
+aluno2 = input('Digite o nome do segundo aluno: ')
+aluno3 = input('Digite o nome do terceiro aluno: ')
+aluno4 = input('Digite o nome do quarto aluno: ')
+
+listaAlunos = [aluno1,aluno2,aluno3,aluno4]
+random.shuffle(listaAlunos)
+
+print(f'A ordem de apresentação é {listaAlunos}')
