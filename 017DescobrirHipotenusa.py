f96ea407e47bba8f2738497b5fee0a3a04bacb06
RicardoMagalhaesRFinkelstein <ricardomrfin@gmail.com>
2022-07-29 12:20:10 -0300

    Exercício de hipotenusa resolvido

--- 017DescobrirHipotenusa.py
+++ 017DescobrirHipotenusa.py
@@ -1,2 +1,10 @@
 #Ler os catetos de um triângulo retângulo e retornar a hipotenusa
+import math
+catOp = float(input('Digite o comprimento do cateto oposto: '))
+catAdj = float(input('Digite o comprimento do cateto adjacente: '))
 
+hipo = math.hypot(catOp, catAdj)
+
+print(f'A hipotenusa vale {hipo:.2f}')
+
+math.
