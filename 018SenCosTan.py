3b9f61f741561f3ab18fa24e9d2573b669a70a95
RicardoMagalhaesRFinkelstein <ricardomrfin@gmail.com>
2022-07-29 12:41:04 -0300

    Exercício de seno, cosseno e tangente resolvido

--- 018SenCosTan.py
+++ 018SenCosTan.py
@@ -1,1 +1,7 @@
 #Ler um ângulo qualquer e mostrar o seno, cosseno e tangente desse ângulo
+
+import math
+angulo = float(input('Digite o ângulo desejado: '))
+angulo = math.radians(angulo)
+print(f'O SENO vale {math.sin(angulo):.2f}\nO COSSENO vale {math.cos(angulo):.2f}\nPor fim, a TANGENTE vale {math.tan(angulo):.2f}')
+
